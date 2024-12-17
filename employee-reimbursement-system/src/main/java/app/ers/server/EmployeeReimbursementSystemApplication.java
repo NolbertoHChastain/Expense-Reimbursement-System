1e32d07432138f5594bfc544e0f756d1a74b4e94
NolbertoHChastain <berto4chastain@gmail.com>
2024-12-17 17:40:35 -0500

    initial commit

---
+++ employee-reimbursement-system/src/main/java/app/ers/server/EmployeeReimbursementSystemApplication.java
@@ -0,0 +1,13 @@
+package app.ers.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeReimbursementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeReimbursementSystemApplication.class, args);
+	}
+
+}
---
+++ employee-reimbursement-system/src/test/java/app/ers/server/EmployeeReimbursementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ers.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeReimbursementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
