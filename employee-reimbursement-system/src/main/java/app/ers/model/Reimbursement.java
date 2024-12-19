2dcb97f1400826d5a71e4c2b9635a457fd441807
NolbertoHChastain <berto4chastain@gmail.com>
2024-12-18 22:18:05 -0500

    updated boilerplate code

--- employee-reimbursement-system/src/main/java/app/ers/model/Reimbursement.java
+++ employee-reimbursement-system/src/main/java/app/ers/model/Reimbursement.java
@@ -40,7 +40,6 @@ public class Reimbursement {
 
     // boiler plate - no args, all args, getter/setter, toString
 
-
     public Reimbursement() {
     }
 
@@ -50,5 +49,56 @@ public class Reimbursement {
         this.amount = amount;
         this.status = status;
         this.user = user;
+    }
+
+    public int getReimbId() {
+        return reimbId;
+    }
+
+    public void setReimbId(int reimbId) {
+        this.reimbId = reimbId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Reimbursement{" +
+                "reimbId=" + reimbId +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", status='" + status + '\'' +
+                ", user=" + user +
+                '}';
     }
 }
